2f4564a7adda85659319a451d73169ec7c40ec20
Anton Kalnyy <a.kalnoy@gmail.com>
2023-10-27 18:53:46 -0400

    first commit

---
+++ WebDriverManagerTest.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service as ChromeService
+#from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+import time
+
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("https://google.com")
+time.sleep(3)
+driver.close()
+driver.quit()
